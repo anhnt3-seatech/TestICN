c14c708299767a01866079d1cba17838d9cbc3af
anhnt3-seatech <anhnt3.seatech@gmail.com>
2025-10-28 14:16:35 +0700

    c

--- src/com/vn/test/TestAction.java
+++ src/com/vn/test/TestAction.java
@@ -36,7 +36,7 @@ public class TestAction extends PluginAction {
 	 * @return A short description of the action.
 	 */
 	public String getName(Locale locale) {
-		return "TestAction31";
+		return "TestAction32";
 	}
 
 	/**
